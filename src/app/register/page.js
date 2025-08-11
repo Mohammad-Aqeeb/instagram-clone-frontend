fe78856dadccb47b5a1fe5ce5c2d10dbb9c78e86
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-08-11 16:04:55 +0530

    change in register page

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,15 +1,22 @@
+import Link from 'next/link';
 import styles from './register.module.css';
 
 export default function RegisterPage() {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Sign Up</h1>
-      <form className={styles.form}>
-        <input type="text" placeholder="Username" className={styles.input} />
-        <input type="email" placeholder="Email" className={styles.input} />
-        <input type="password" placeholder="Password" className={styles.input} />
-        <button type="submit" className={styles.button}>Register</button>
-      </form>
+    <div className={styles.wrapper}>
+      <div className={styles.card}>
+        <h1 className={styles.logo}>Instagram</h1>
+        <form className={styles.form}>
+          <input type="text" placeholder="Email" className={styles.input} />
+          <input type="text" placeholder="Full Name" className={styles.input} />
+          <input type="text" placeholder="Username" className={styles.input} />
+          <input type="password" placeholder="Password" className={styles.input} />
+          <button type="submit" className={styles.button}>Sign up</button>
+        </form>
+        <p className={styles.text}>
+          Have an account? <Link href="/login" className={styles.link}>Log in</Link>
+        </p>
+      </div>
     </div>
   );
 }
