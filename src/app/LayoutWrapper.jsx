f0078ed623ec1ad5b6e951358bbdc7ee61c05c2d
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-08-11 16:01:30 +0530

    change in layoutWrapper

--- src/app/LayoutWrapper.jsx
+++ src/app/LayoutWrapper.jsx
@@ -1,11 +1,26 @@
 'use client';
 
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import BottomNav from '@/components/BottomNav/BottomNav';
+import { useEffect, useState } from 'react';
+import Spinner from '@/components/Spinner/Spinner';
 
 export default function LayoutWrapper({ children }) {
+
+  const router = useRouter();
   const pathname = usePathname();
-  const showBottomNav = pathname !== '/';
+  const showBottomNav = pathname !== '/' && pathname !== '/register' && pathname !== '/login';
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(()=>{
+    const token = localStorage.getItem('token');
+    if(token){
+      router.replace('/feed');
+    }
+    setLoading(false)
+  },[])
+
+  if(loading) return <Spinner/>
 
   return (
     <>
